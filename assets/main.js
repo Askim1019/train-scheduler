46cff3f92729b6e1f4b7c5ffb0d55f0c93fd937f
Alex Kim <askim1019@gmail.com>
2018-04-29 14:18:02 -0500

    Add firebase variable and other variables to the clikc function

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
- // Initialize Firebase
+ // Initialize Firebase with special api key and config
  var config = {
     apiKey: "AIzaSyDUilhun_95xoPLV1uh2MeWwf9_RlBtXfE",
     authDomain: "train-scheduler-5e8ed.firebaseapp.com",
@@ -7,4 +7,18 @@
     storageBucket: "train-scheduler-5e8ed.appspot.com",
     messagingSenderId: "1027948051918"
   };
+
   firebase.initializeApp(config);
+
+
+  // Make the firebase database call simplified by assignment
+  var database = firebase.database();
+
+  $("#add-data").click(function(){
+    var trainName = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var frequency = $("#frequency").val().trim();
+    var firstTrain = $("#firstTrainTime").val().trim();
+    var nextArrival;
+    var minutesAway;
+  });
