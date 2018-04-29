bad24d48a2de283452aa2024a047e30bd500272c
Alex Kim <askim1019@gmail.com>
2018-04-28 22:56:15 -0500

    Add the skeleton

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDUilhun_95xoPLV1uh2MeWwf9_RlBtXfE",
+    authDomain: "train-scheduler-5e8ed.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5e8ed.firebaseio.com",
+    projectId: "train-scheduler-5e8ed",
+    storageBucket: "train-scheduler-5e8ed.appspot.com",
+    messagingSenderId: "1027948051918"
+  };
+  firebase.initializeApp(config);
